dfb760bbedfaa113d53266dde5e64ca028f75c0f
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-09-21 11:25:07 -0300

    first commit

---
+++ src/main/java/com/essia/arq_virtuais/ArqVirtuaisApplication.java
@@ -0,0 +1,13 @@
+package com.essia.arq_virtuais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqVirtuaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqVirtuaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/essia/arq_virtuais/ArqVirtuaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essia.arq_virtuais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqVirtuaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
